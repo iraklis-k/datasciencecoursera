d999222a72994c2eb422b40ffce5c1dc8dae9188
iraklis-k <iraklisk@me.com>
2015-04-25 18:14:58 +1000

    Progress. Removed obsolete function stripName()

--- getDataProject/run_analysis.R
+++ getDataProject/run_analysis.R
@@ -27,23 +27,6 @@
 #  one each for 'train' and 'test', so that no effort is duplicated.
 #  Then I will append one data frame to the end of the other. 
 
-stripName <- function(str, sub){
-    # Strip the descriptor of a measurement from the filename
-
-    library(tools) # for stripping file extensions
-    
-    # First strip path
-    str <- tail(strsplit(str, '/')[[1]], n=1)
-    
-    # Now strip filename
-    str <- file_path_sans_ext(str)
-
-    # And now strip the '_test' etc to normalise names
-    str <- gsub(paste('_', sub, sep=''), '', str)
-
-    return(str)
-}
-
 makeDF <- function(dir){
     # This function collates all data into a single data frame
 
@@ -51,26 +34,23 @@ makeDF <- function(dir){
     listing <- list.files(dir, full.names = TRUE)
 
     # Initialise a data frame via 'subject_test'
-    #nData <- dim(read.csv(listing[2]))[1]
-    #df <- data.frame(index = 1:nData)
     print(paste('Adding contents of', listing[2]))
-    df <- read.table(listing[2], header=FALSE)
-    
-    # Column-bind each file (dataset). Peculiarities do not loop. 
-    #df <- cbind(df,read.csv(listing[2], header=FALSE))
+    df <- read.table(listing[2], header=FALSE, 
+                     col.names = "subject")
 
+    # Column-bind other files (datasets)
     print(paste('Adding contents of', listing[4]))
-    df <- cbind(df,read.table(listing[4], header=FALSE))
+    df <- cbind(df, read.table(listing[4], header=FALSE, 
+                               col.names = "activity"))
+    
+    # The measurements (X) file is not as simple
+    X <- read.csv(listing[3], sep=' ', header=FALSE)
 
-    print(dim(df))
-    
+    # No need to delete the leading NA column, just ignore
     print(paste('Adding contents of', listing[3]))
-    df <- cbind(df,read.csv(listing[3], sep='  ', header=FALSE))        
+    df <- cbind(df, mean = rowMeans(X, na.rm=TRUE), 
+                stDev = apply(X, 1, sd, na.rm=TRUE))
     
-    # Loop and name each variable after the filename
-    for(i in 2:length(listing)){
-        names(df)[i] <- stripName(listing[i], dir)
-    }
     return(df)
 }
 
@@ -81,18 +61,14 @@ train <- makeDF('train')
 # And tack one data frame onto the end of the other
 allData <- rbind(test, train)
 
-# Finally, give the data frame some descriptive column names
-names(allData)[3] <- "measurement"
-names(allData)[4] <- "activity_code"
-
 # *** The above fulfils steps [1] through [4] of the project ***
 
 # For step [5] let's filter the data by activity and then subject
 library(dplyr)
 
 #newData <- arrange(allData, allData$activity_code, allData$subject)
-nActivities <- unique(allData$activity_code)
-averages <- data.frame(activity = unique(allData$activity_code))
+# nActivities <- unique(allData$activity_code)
+# averages <- data.frame(activity = unique(allData$activity_code))
 
 # a <- filter(allData, activity_code==1)
 # b <- a$measurement
