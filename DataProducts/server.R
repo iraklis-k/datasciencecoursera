e42d459f3939a026a6ca7be765abbc6f6d56e95c
Iraklis Konstantopoulos <iraklisk@me.com>
2015-08-06 16:14:03 +1000

    Added Shiny project files
    As it stands this code will not find the data files required to populate the plots. Those will be added in the next commit.

---
+++ DataProducts/server.R
@@ -0,0 +1,43 @@
+# server.R
+# 
+# Programme script for PowerFactor shiny dashboard; see wrapper for docs
+# 
+# Iraklis Konstantopoulos
+# Sydney, 05 August 2015
+# 
+
+library(shiny, gridExtra)
+
+# Input consumption data
+#dat <- read.csv('PowerFactor.csv')
+dat <- read.csv('MultiMeter.csv')
+
+
+shinyServer(function(input, output) {
+
+    # Plot 1: Consumption Time Series
+    output$TimeSeries <- renderPlot({
+
+        # Restrict the data frame to the date selected
+        plotFrame <- dat[as.Date(dat$Date) == as.Date(input$Date),]
+
+        # Get the index of the column to plot
+        myCol <- grep(input$Meter, names(plotFrame))[1]
+
+        # ggplot the data: kWh and PF
+        qplot(x=as.list(plotFrame[16])[[1]], 
+                     y=as.list(plotFrame[myCol])[[1]],
+                     xlab='Time [hh:mm:ss]', ylab='Consumption [kWh]', 
+                     main = paste('Meter ', input$Meter), asp=0.5)
+
+        #ggplot() + 
+        #  geom_dotplot(aes(y=as.list(plotFrame[myCol])[[1]]))
+        
+        # Also need to set up the four summary boxes
+        
+        # I NEED TO REORGANISE THE DF. NO GOOD! 
+        
+    })
+})
+
+
---
+++ DataProducts/ui.R
@@ -0,0 +1,34 @@
+# ui.R 
+# 
+# Interface script for PowerFactor shiny dashboard; see wrapper for docs
+# 
+# Iraklis Konstantopoulos
+# Sydney, 05 August 2015
+# 
+
+library(shiny)
+# Read the data frame to get the list of meters
+nm <- names(read.csv('MultiMeter.csv'))
+
+# First step to unique names: kWh column names
+meter_names = nm[grep('kWh', nm)]  
+
+# Split on kWh and keep only the meter number
+for(i in 1:4){ 
+  meter_names[i] = strsplit(meter_names, '_')[[i]][2]
+  }
+
+shinyUI(pageWithSidebar(
+  headerPanel("Consumed vs Delivered Energy"), 
+  sidebarPanel(
+
+    dateInput("Date", "Date:", 
+              value='2015-01-01', format='yyyy-mm-dd'),
+    
+    selectInput("Meter", "Meter:", choices=meter_names)
+  ),
+  mainPanel(
+    plotOutput("TimeSeries"),
+    plotOutput("Geomapper")
+  )
+))
