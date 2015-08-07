d9c91d8ea4759bf775fe9a5f847500e6fb4b422d
Iraklis Konstantopoulos <iraklisk@me.com>
2015-08-07 13:48:22 +1000

    Fixed a time zone bug
    Now the x-axis is ticked with a glorious display of format "%H:%M"

--- DataProducts/Shiny/server.R
+++ DataProducts/Shiny/server.R
@@ -6,7 +6,8 @@
 # Sydney, 05 August 2015
 # 
 
-library(shiny, gridExtra)
+library(shiny, grid)
+library(scales)
 
 # Input consumption data
 kWh <- read.csv('kWh.csv')
@@ -29,19 +30,25 @@ shinyServer(function(input, output) {
         myCol <- grep(input$Meter, names(plotkWh))[1]
         
         # And further prune the data frame to only include plot data
-        plotFrame <- data.frame(plotkWh[myCol], plotkVAh[myCol], plotkWh[6])
+        plotFrame <- data.frame(plotkWh[myCol], plotkVAh[myCol], 
+                  as.POSIXct(plotkWh$Date_Time, tz="UTC", 
+                             format="%Y-%m-%d %H:%M:%S"))
         names(plotFrame) <- c("kWh", "kVAh", "Time")
-        
+        timeTicks <- substring(as.character(plotFrame$Time), 
+                               12, 16)[seq(1, length(timeTicks), 8)]
+        print(timeTicks)
         # ggplot the data: kWh and kVAh (scatter), and PF (line)
         #qplot(Time, kWh, data=plotFrame)
         ggplot(plotFrame, aes(Time)) +
-          geom_point(aes(y=kWh)) +
-          geom_point(aes(y=kVAh)) #+
-          #geom_line(aes(y=kWh/kVAh))        
-                
+          geom_point(size=3, alpha=0.5, colour="blue", aes(y=kWh)) +
+          geom_point(size=10, shape="*", colour="darkgrey", aes(y=kVAh)) +
+          #geom_line(aes(y=kWh/kVAh)) +
+          theme(axis.text.x=element_text(angle=30)) + 
+          scale_x_datetime(breaks = "2 hour", labels=date_format("%H:%M"))
+
         # Also need to set up the four summary boxes
 
-    })
+  })
 })
 
 
