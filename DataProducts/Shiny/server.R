b77a1dc1c182928278c1035385b51d9de56d27ef
Iraklis Konstantopoulos <iraklisk@me.com>
2015-08-07 15:16:34 +1000

    Added Power Factor plot

--- DataProducts/Shiny/server.R
+++ DataProducts/Shiny/server.R
@@ -6,7 +6,8 @@
 # Sydney, 05 August 2015
 # 
 
-library(shiny, grid)
+library(shiny)
+library(gridExtra)
 library(scales)
 
 # Input consumption data
@@ -34,18 +35,35 @@ shinyServer(function(input, output) {
                   as.POSIXct(plotkWh$Date_Time, tz="UTC", 
                              format="%Y-%m-%d %H:%M:%S"))
         names(plotFrame) <- c("kWh", "kVAh", "Time")
-        timeTicks <- substring(as.character(plotFrame$Time), 
-                               12, 16)[seq(1, length(timeTicks), 8)]
-        print(timeTicks)
-        # ggplot the data: kWh and kVAh (scatter), and PF (line)
-        #qplot(Time, kWh, data=plotFrame)
-        ggplot(plotFrame, aes(Time)) +
+
+        # Decimal places in y tick label
+        fmt <- function(){
+          function(x) format(x,nsmall = 2,scientific = FALSE)
+        }
+                
+        # Plot kWh and kVAh (scatter), and PF (line)
+        g1 <- ggplot(plotFrame, aes(Time)) +
+          #coord_fixed(ratio=1000) + 
           geom_point(size=3, alpha=0.5, colour="blue", aes(y=kWh)) +
           geom_point(size=10, shape="*", colour="darkgrey", aes(y=kVAh)) +
-          #geom_line(aes(y=kWh/kVAh)) +
+          theme(axis.text.x = element_blank()) + 
+          theme(axis.title.x = element_blank()) + 
+          theme(plot.margin = unit(c(0., 0., 0., 0.34), "cm")) +
+          scale_x_datetime(breaks = "2 hour")
+        
+        g2 <- ggplot(plotFrame, aes(Time)) + 
+          geom_line(aes(y=(kWh/kVAh)), size=1, colour='#9b57a2') + 
+          #coord_fixed(ratio=350000) + 
+          scale_y_continuous(labels=fmt()) + 
+          ylab("Power Factor") + 
+          #ylim(c(0.9*min(kWh/kVAh), 1.)) + 
           theme(axis.text.x=element_text(angle=30)) + 
+          theme(plot.margin = unit(c(0., 0., 0., 0.), "cm")) +
           scale_x_datetime(breaks = "2 hour", labels=date_format("%H:%M"))
-
+        
+        grid.arrange(g1, g2)#, 
+                     #widths = unit(c(20,20), "cm"),
+                     #heights = unit(rep(10, 2), "cm"))
         # Also need to set up the four summary boxes
 
   })
